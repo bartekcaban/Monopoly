488dc5c863f5cc9a30e1381f7a9a43a44dfdd984
Jakub Lyko <kubeckk21@gmail.com>
2019-05-21 20:16:15 +0200

    still not working

--- Monopoly/Assets/Scripts/SwitcherImageOnClickDetection.cs
+++ Monopoly/Assets/Scripts/SwitcherImageOnClickDetection.cs
@@ -8,7 +8,7 @@ public class SwitcherImageOnClickDetection : MonoBehaviour, IPointerClickHandler
 {
     DialogMenu dialogMenu;
     public Game game;
-    private List<Texture> chosenTextures;
+    private Texture[] textures;
     private int currentTextureIndex;
 
     // Start is called before the first frame update
@@ -16,7 +16,7 @@ public class SwitcherImageOnClickDetection : MonoBehaviour, IPointerClickHandler
     {
         dialogMenu = DialogMenu.Instance();
         GameUI gameUIScript = GameObject.Find("GameUIManager").GetComponent<GameUI>();
-        chosenTextures = gameUIScript.chosenTextures;
+        textures = gameUIScript.textures;
         currentTextureIndex = gameUIScript.currentTextureIndex;
     }
 
@@ -29,9 +29,9 @@ public class SwitcherImageOnClickDetection : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("I'm in OnPointerClick");
-        var currentlyShowedProperty = game.currentPlayer.ownedProperties.FirstOrDefault(x => x.propertyName.ToLower() == chosenTextures[currentTextureIndex].name.ToLower());
-        Debug.Log("chosenTextures: " + chosenTextures.Count);
-        Debug.Log("currentlyShowedProperty name: " + currentlyShowedProperty.propertyName);
+        Debug.Log("currentTextureIndex: " + currentTextureIndex);
+        Property currentlyShowedProperty = game.currentPlayer.ownedProperties.FirstOrDefault(x => x.propertyName.ToLower() == textures[currentTextureIndex].name.ToLower());
+        if (currentlyShowedProperty) Debug.Log("currentlyShowedProperty name: " + currentlyShowedProperty.propertyName); else Debug.Log("currentlyShowedProperty not found.");
         dialogMenu.ShowForPropertyOwner(currentlyShowedProperty);
     }
 }
